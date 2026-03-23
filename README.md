2f25afecea97ae8141bdbd5bf3112c88f440dde7
mengan-tech <menganhan763@gmail.com>
2026-03-23 10:05:41 +0700

    first commit

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # I3-typing_master-2026
 
 - download java-jdk 17 or 21
+- download and install the java jdk from oracle website
+- https://www.oracle.com/java/technologies/downloads/
 - set up vs code
 - connect java-jdk to system in environment
 - then download git and create account in GitHub
 - you can write code in vs code and local.
+
+compile it: javac welcome_menu.java
+run it: java welcome_menu.java
---
+++ welcome_menu.java
@@ -0,0 +1,13 @@
+
+class Welcome_menu {
+    public static void main(String[] args) {
+        System.out.println("------- Welcome to I3 Typing Master v2026 -----------");
+        System.out.println("To begin, please select one of the following options:");
+        System.out.println("1. Login");
+        System.out.println("2. Register");
+        System.out.println("3. Exit");
+        System.out.print("Choose an option: -");
+        
+        // Add more options as needed
+    }
+}
